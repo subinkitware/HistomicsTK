883f8e8cd168178836f7cc7749c39f2b28fb2515
Subin Erattakulangara <136746896+subinkitware@users.noreply.github.com>
2023-07-07 11:10:03 -0400

    record parameters and primary results in annotation metadata (#999)
    * added new annotation info

--- histomicstk/cli/NucleiDetection/NucleiDetection.py
+++ histomicstk/cli/NucleiDetection/NucleiDetection.py
@@ -380,12 +380,13 @@ def main(args):
     print('\n>> Writing annotation file ...\n')
 
     annotation = {
-        'name': 'NucleiDetection',
+        'name': annot_fname + '-nuclei-' + args.nuclei_annotation_format,
         'elements': nuclei_list,
-        'src_mu_lab': src_mu_lab,
-        'src_sigma_lab': src_sigma_lab,
-        'inverted_image': invert_image or default_img_inversion,
-        'input_parameters': args
+        'attributes': {
+            'src_mu_lab': None if src_mu_lab is None else src_mu_lab.tolist(),
+            'src_sigma_lab': None if src_sigma_lab is None else src_sigma_lab.tolist(),
+            'params': vars(args),
+        },
     }
 
     with open(args.outputNucleiAnnotationFile, 'w') as annotation_file:
@@ -398,5 +399,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    
     main(CLIArgumentParser().parse_args())
